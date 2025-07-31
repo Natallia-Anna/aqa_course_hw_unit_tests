fefc22302514e4d17cacc3b5722ebe17d06078dc
Natallia-Anna <moyseencko-natalya@yandex.ru>
2025-07-31 22:56:08 +0300

    added 3 files task-1.js task-2.js task-3.js

--- homework-solutions/js/hw-4/task-2.js
+++ homework-solutions/js/hw-4/task-2.js
@@ -14,3 +14,25 @@
   5. Проверить задание со следующими значениями в переменной age: 10, 17, 18, 19, 59, 60, 61
 
   */
+
+  let minAge = 18;
+let maxAge = 60;
+
+// Тестовые значения
+let testAges = [10, 17, 18, 19, 59, 60, 61, "18", "двадцать", true, null, -1];
+
+for (let age of testAges) {
+  console.log(`\nПроверка возраста: ${age}`);
+
+  if (typeof age !== "number") {
+    console.log("Incorrect data type");
+  } else if (age < minAge && age > 0) {
+    console.log("You don't have access cause your age is " + age + " It's less then " + minAge);
+  } else if (age >= minAge && age <= maxAge) {
+    console.log("Welcome!");
+  } else if (age > maxAge) {
+    console.log("Keep calm and look Culture channel");
+  } else {
+    console.log("Technical work");
+  }
+}
--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -6,3 +6,31 @@
   преобразовываясь в number
 
 */
+
+let minAge = 18;
+let maxAge = 60;
+
+// Тестовые значения
+let testAges = [10, 17, 18, 19, 59, 60, 61, "18", "2", "abc", "20a", true, null, undefined];
+
+for (let input of testAges) {
+  console.log(`\nПроверка значения: ${input}`);
+
+  // Преобразуем строку, если она содержит только цифры
+  if (typeof input === "string" && /^\d+$/.test(input)) {
+    input = Number(input);
+  }
+
+  // Проверка типа данных после возможного преобразования
+  if (typeof input !== "number" || isNaN(input)) {
+    console.log("Incorrect data type");
+  } else if (input < minAge && input > 0) {
+    console.log("You don't have access cause your age is " + input + ". It's less than " + minAge);
+  } else if (input >= minAge && input <= maxAge) {
+    console.log("Welcome!");
+  } else if (input > maxAge) {
+    console.log("Keep calm and look Culture channel");
+  } else {
+    console.log("Technical work");
+  }
+}
