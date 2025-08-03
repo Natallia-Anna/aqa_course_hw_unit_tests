5823a41df8d674b9e78678b0ab66a642c2386472
Natallia-Anna <moyseencko-natalya@yandex.ru>
2025-08-03 20:40:11 +0300

    added 3 files task-1.js task-2.js task-3.js

--- homework-solutions/js/hw-5/task-1.js
+++ homework-solutions/js/hw-5/task-1.js
@@ -5,7 +5,13 @@
  */
 
 let evenNumbersResult = '';
-
+for (let i = 10; i >= 0; i -= 2) {
+  evenNumbersResult += i;
+  if (i !== 0) {
+    evenNumbersResult += '-';
+  }
+}
+console.log(evenNumbersResult)
 /**
  * Создать строку из 5 строк с увеличивающимся количеством смайликов ":)".
  * Переменная для результата `smilePatternResult` уже создана и содержит пустую строку.
@@ -18,13 +24,22 @@ let evenNumbersResult = '';
  */
 
 let smilePatternResult = '';
+let smile = ':)';
+for (let i = 1; i <= 5; i++) {
+  smilePatternResult += smile.repeat(i);
+  if (i < 5) {
+    smilePatternResult += '\n'; // переход на новую строку
+  }
+}
+console.log(smilePatternResult)
 
 /**
  * Заменить все пробелы в переменной text на "1".
  * Переменная для результата `replaceSpacesWithOneResult` уже создана и содержит пустую строку.
  * Ожидаемый результат: 'Hello!1I1am1a1JS1student!'
  */
-const text = 'Hello! I am a JS student!';
-let replaceSpacesWithOneResult = '';
 
-export { evenNumbersResult, smilePatternResult, replaceSpacesWithOneResult };
+const text = 'Hello! I am a JS student!';
+
+let replaceSpacesWithOneResult = text.replace(/ /g, '1');
+console.log(replaceSpacesWithOneResult);
