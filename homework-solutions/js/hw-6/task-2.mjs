1bd4d1a7326582d063673fe6927a38a350b92fc5
Natallia-Anna <moyseencko-natalya@yandex.ru>
2025-08-07 20:04:30 +0300

    addet 3 files hw-6

--- homework-solutions/js/hw-6/task-2.mjs
+++ homework-solutions/js/hw-6/task-2.mjs
@@ -13,7 +13,31 @@
   const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
 */
 
+
+const competitorPizzas = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
+const myPizzasT1 = ['Peperoni', 'Margherita', 'Diablo', 'Vegetarian'];
+const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
+
 let resultUnique;
 let resultNull;
 
-export { resultNull, resultUnique };
+function findUniquePizzas(myPizzas, competitorPizzas) {
+  // Приводим все названия к нижнему регистру для сравнения
+  const competitorLower = competitorPizzas.map(pizza => pizza.toLowerCase());
+  
+  const unique = myPizzas.filter(pizza => 
+    !competitorLower.includes(pizza.toLowerCase())
+  );
+
+  if (unique.length === 0) {
+    return null;
+  }
+
+  return unique;
+}
+
+// Пример использования:
+resultUnique = findUniquePizzas(myPizzasT1, competitorPizzas); // ['Margherita', 'Vegetarian']
+resultNull = findUniquePizzas(myPizzasT2, competitorPizzas);   // null
+
+console.log(resultNull, resultUnique);
